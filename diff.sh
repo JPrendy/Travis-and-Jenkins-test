fb29736135b27ae66da9824846c639f8e966bdad
James Prendergast <jamprendergast@gmail.com>
2019-05-19 16:26:51 +0100

    updates to the sh file to match how it would be like in the final version

--- diff.sh
+++ diff.sh
@@ -2,10 +2,10 @@
 
 #the following would be bin/sh in the jenkins file
 set -x
-variable=$(git diff origin/master... | grep "README");
+variable=$(git diff origin/master... | grep "Support/");
 if [ -z "$variable" ]; then
-     echo "changes were not made to the README"
+     echo "changes were not made in the Support Directory"
 else
-     echo "changes were made to the README"
+     echo "changes were made in the Support Directory"
 fi
 
