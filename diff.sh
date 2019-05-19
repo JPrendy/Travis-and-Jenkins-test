33416483a9457148c989a98d374a0a6c8c33c2fa
James Prendergast <jamprendergast@gmail.com>
2019-05-19 13:40:47 +0100

    continued work on the git diff

--- diff.sh
+++ diff.sh
@@ -2,6 +2,8 @@
 set -x
 variable=$(git diff origin/master... | grep "README");
 if [ -z "$variable" ]; then
-     ls
+     echo "changes were not made to the README"
+else
+     echo "changes were made to the README"
 fi
 
